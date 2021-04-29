9388f336d1965c179c8deaad85bba9efe7d75d0e
Ludovic Henry <git@ludovic.dev>
2021-04-29 11:56:04 +0200

    Add support for downloading release asset content

--- src/main/java/dev/ludovic/netlib/website/ReleaseController.java
+++ src/main/java/dev/ludovic/netlib/website/ReleaseController.java
@@ -4,8 +4,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.List;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,5 +31,27 @@ public class ReleaseController {
     return new Release.Finder()
                 .restTemplate(restTemplate)
                 .all();
+  }
+
+  @GetMapping("/releases/assets/{asset_id}")
+  @CrossOrigin(origins = "*")
+  public ReleaseAsset releasesAssets(@PathVariable int asset_id) {
+    return new ReleaseAsset.Finder()
+                .restTemplate(restTemplate)
+                .findById(asset_id);
+  }
+
+  @GetMapping(
+    value = "/releases/assets/{asset_id}",
+    produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+  @CrossOrigin(origins = "*")
+  public byte[] releasesAssetsContent(@PathVariable int asset_id) {
+    var asset = new ReleaseAsset.Finder()
+                    .restTemplate(restTemplate)
+                    .findById(asset_id);
+
+    return restTemplate.execute(asset.getBrowser_download_url(), HttpMethod.GET, null, response -> {
+      return StreamUtils.copyToByteArray(response.getBody());
+    });
   }
 }
--- src/main/java/dev/ludovic/netlib/website/models/ReleaseAsset.java
+++ src/main/java/dev/ludovic/netlib/website/models/ReleaseAsset.java
@@ -1,13 +1,20 @@
 
 package dev.ludovic.netlib.website;
 
+import java.util.Map;
+
 public final class ReleaseAsset {
 
+  private int id;
   private String browser_download_url;
   private String name;
   private String content_type;
 
   private ReleaseAsset() {}
+
+  public int getId() {
+    return id;
+  }
 
   public String getBrowser_download_url() {
     return browser_download_url;
@@ -19,5 +26,12 @@ public final class ReleaseAsset {
 
   public String getContent_type() {
     return content_type;
+  }
+
+  public static class Finder extends dev.ludovic.netlib.website.Finder<Finder> {
+
+    public ReleaseAsset findById(int id) {
+      return restTemplate.getForObject("https://api.github.com/repos/luhenry/netlib/releases/assets/{id}", ReleaseAsset.class, Map.of("id", id));
+    }
   }
 }
